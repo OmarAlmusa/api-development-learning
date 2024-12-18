72e6a907e29e673f3fbe83325b78c3ba5d438b6d
Omar Almusa <omaralmusa01@gmail.com>
2024-12-18 16:47:57 +0300

    Stop tracking app/static

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 # from .postgres_ORM import create_DB_and_tables
 # from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-import os
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import HTMLResponse
+# import os
 from .routes import characters, users, auth, vote
 
 # @asynccontextmanager
@@ -29,14 +29,14 @@ app.add_middleware(
 )
 
 #### ======================================= STATIC FILES ===============================================
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get('/', response_class=HTMLResponse)
-async def homepage():
-    html_file_path = os.path.join("app", "static", "index.html")
-    with open(html_file_path, "r") as file:
-        html_content = file.read()
-    return HTMLResponse(content=html_content)
+# @app.get('/', response_class=HTMLResponse)
+# async def homepage():
+#     html_file_path = os.path.join("app", "static", "index.html")
+#     with open(html_file_path, "r") as file:
+#         html_content = file.read()
+#     return HTMLResponse(content=html_content)
 
 
 
